9e7437d121826efa78dcd28583459562a22d11d8
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-16 15:42:16 +0900

    inject

--- app/src/main/java/jp/cordea/tw/MainActivity.kt
+++ app/src/main/java/jp/cordea/tw/MainActivity.kt
@@ -1,23 +1,27 @@
 package jp.cordea.tw
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var subcomponent: MainActivitySubcomponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        subcomponent = (application as App)
+            .appComponent
+            .mainActivitySubcomponentFactory()
+            .create()
+        subcomponent.inject(this)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
