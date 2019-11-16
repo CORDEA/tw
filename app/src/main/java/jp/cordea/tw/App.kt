c875524a24cf4ca50fa77884210c89126e1cc896
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-16 17:40:36 +0900

    initialize

--- app/src/main/java/jp/cordea/tw/App.kt
+++ app/src/main/java/jp/cordea/tw/App.kt
@@ -1,6 +1,11 @@
 package jp.cordea.tw
 
 import android.app.Application
+import android.util.Log
+import com.twitter.sdk.android.core.DefaultLogger
+import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.TwitterConfig
 
 class App : Application() {
     lateinit var appComponent: AppComponent
@@ -11,5 +16,19 @@ class App : Application() {
             DaggerAppComponent
                 .factory()
                 .create(this)
+
+        Twitter.initialize(
+            TwitterConfig
+                .Builder(this)
+                .twitterAuthConfig(
+                    TwitterAuthConfig(
+                        BuildConfig.CONSUMER_KEY,
+                        BuildConfig.CONSUMER_SECRET
+                    )
+                )
+                .logger(DefaultLogger(Log.DEBUG))
+                .debug(true)
+                .build()
+        )
     }
 }
