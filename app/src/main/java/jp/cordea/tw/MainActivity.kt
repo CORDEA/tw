c8c1b7ecc6ed72b376da2b10887463fdc3930b3f
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-17 18:24:47 +0900

    hide toolbar

--- app/src/main/java/jp/cordea/tw/MainActivity.kt
+++ app/src/main/java/jp/cordea/tw/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var subcomponent: MainActivitySubcomponent
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
             .create()
         subcomponent.inject(this)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
