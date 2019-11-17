d98d9125e70ca55298f912b3c1be0ea3db570ff6
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-17 11:33:22 +0900

    make view model injectable

--- app/src/main/java/jp/cordea/tw/ui/home/HomeFragment.kt
+++ app/src/main/java/jp/cordea/tw/ui/home/HomeFragment.kt
@@ -1,24 +1,32 @@
 package jp.cordea.tw.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import jp.cordea.tw.R
+import jp.cordea.tw.*
+import javax.inject.Inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), ViewModelInjectable<HomeViewModel> {
+    @Inject
+    override lateinit var viewModelFactory: ViewModelFactory<HomeViewModel>
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val viewModel by lazy { viewModel() }
+
+    override fun onAttach(context: Context) {
+        (requireActivity() as MainActivity)
+            .subcomponent
+            .homeFragmentSubcomponent()
+            .create()
+            .inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    ): View = inflater.inflate(R.layout.fragment_home, container, false)
 }
--- app/src/main/java/jp/cordea/tw/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/cordea/tw/ui/home/HomeViewModel.kt
@@ -1,13 +1,7 @@
 package jp.cordea.tw.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+class HomeViewModel @Inject constructor() : ViewModel() {
 }
