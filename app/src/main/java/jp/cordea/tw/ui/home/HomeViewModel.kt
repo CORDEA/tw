31ac60d3ec021020f48584108163d7e99f3d9ce9
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-17 12:37:34 +0900

    fetch tweets

--- app/src/main/java/jp/cordea/tw/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/cordea/tw/ui/home/HomeViewModel.kt
@@ -1,7 +1,34 @@
 package jp.cordea.tw.ui.home
 
 import androidx.lifecycle.ViewModel
+import jp.cordea.tw.StatusesRepository
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
-class HomeViewModel @Inject constructor() : ViewModel() {
+@ExperimentalCoroutinesApi
+class HomeViewModel @Inject constructor(
+    private val repository: StatusesRepository
+) : ViewModel(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext = Dispatchers.Main + job
+
+    private val _onData by lazy {
+        val channel = ConflatedBroadcastChannel<List<HomeListItemModel>>()
+        launch {
+            repository.findAll(null)
+                .map { it.map { tweet -> HomeListItemModel(tweet.text) } }
+                .collect { channel.send(it) }
+        }
+        channel
+    }
+    val onData get() = _onData.openSubscription()
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
