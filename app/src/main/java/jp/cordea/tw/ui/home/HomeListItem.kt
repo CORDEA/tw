32592dd6bbbe8b501d55d44d21f2a232f95fb52d
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-17 12:12:33 +0900

    add list item

---
+++ app/src/main/java/jp/cordea/tw/ui/home/HomeListItem.kt
@@ -0,0 +1,17 @@
+package jp.cordea.tw.ui.home
+
+import com.xwray.groupie.databinding.BindableItem
+import jp.cordea.tw.R
+import jp.cordea.tw.databinding.HomeListItemBinding
+
+class HomeListItemModel(
+    val body: String
+)
+
+class HomeListItem(private val model: HomeListItemModel) : BindableItem<HomeListItemBinding>() {
+    override fun getLayout(): Int = R.layout.home_list_item
+
+    override fun bind(viewBinding: HomeListItemBinding, position: Int) {
+        viewBinding.model = model
+    }
+}
