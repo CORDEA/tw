d16959686a414fbbb6522ebdcb7aa0edbac16a03
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-17 12:37:44 +0900

    display items

--- app/src/main/java/jp/cordea/tw/ui/home/HomeFragment.kt
+++ app/src/main/java/jp/cordea/tw/ui/home/HomeFragment.kt
@@ -6,14 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import jp.cordea.tw.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class HomeFragment : Fragment(), ViewModelInjectable<HomeViewModel> {
+@ExperimentalCoroutinesApi
+class HomeFragment : Fragment(),
+    CoroutineScope by MainScope(),
+    ViewModelInjectable<HomeViewModel> {
     @Inject
     override lateinit var viewModelFactory: ViewModelFactory<HomeViewModel>
 
     private val viewModel by lazy { viewModel() }
+    private val adapter = GroupAdapter<GroupieViewHolder>()
 
     override fun onAttach(context: Context) {
         (requireActivity() as MainActivity)
@@ -29,4 +41,16 @@ class HomeFragment : Fragment(), ViewModelInjectable<HomeViewModel> {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.adapter = adapter
+
+        launch {
+            viewModel.onData
+                .consumeEach { data ->
+                    adapter.addAll(data.map { HomeListItem(it) })
+                }
+        }
+    }
 }
