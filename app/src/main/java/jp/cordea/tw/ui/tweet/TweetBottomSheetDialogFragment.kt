31f3a32509bbb53ed09cc87b891ed3ac9da778d4
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-23 13:29:31 +0900

    add bottom sheet

---
+++ app/src/main/java/jp/cordea/tw/ui/tweet/TweetBottomSheetDialogFragment.kt
@@ -0,0 +1,16 @@
+package jp.cordea.tw.ui.tweet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import jp.cordea.tw.R
+
+class TweetBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.tweet_bottom_sheet_dialog_fragment, container, false)
+}
