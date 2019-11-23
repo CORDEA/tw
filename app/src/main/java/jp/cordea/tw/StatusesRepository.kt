bf1a1e5959764afe945c2c95affe340a17d4b726
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-23 13:02:28 +0900

    post tweet

--- app/src/main/java/jp/cordea/tw/StatusesRepository.kt
+++ app/src/main/java/jp/cordea/tw/StatusesRepository.kt
@@ -1,23 +1,25 @@
 package jp.cordea.tw
 
 import com.twitter.sdk.android.core.TwitterCore
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class StatusesRepository @Inject constructor() {
+    private val service by lazy {
+        TwitterCore.getInstance()
+            .apiClient
+            .statusesService
+    }
+
     @ExperimentalCoroutinesApi
     fun findAll(count: Int, maxId: Long?) =
         flow {
             emit(
-                TwitterCore.getInstance()
-                    .apiClient
-                    .statusesService
+                service
                     .homeTimeline(count, null, maxId, false, false, false, true)
                     .execute()
             )
@@ -27,6 +29,33 @@ class StatusesRepository @Inject constructor() {
                     StatusesResult.Failure(it.errorBody()!!.string())
                 } else {
                     StatusesResult.Success(it.body()!!)
+                }
+            }
+
+    @ExperimentalCoroutinesApi
+    fun update(status: String) =
+        flow {
+            emit(
+                service
+                    .update(
+                        status,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                    .execute()
+            )
+        }
+            .map {
+                if (it.body() == null) {
+                    StatusesUpdateResult.Failure(it.errorBody()!!.string())
+                } else {
+                    StatusesUpdateResult.Success
                 }
             }
 }
--- app/src/main/java/jp/cordea/tw/StatusesResult.kt
+++ app/src/main/java/jp/cordea/tw/StatusesResult.kt
@@ -6,3 +6,8 @@ sealed class StatusesResult {
     class Success(val tweets: List<Tweet>) : StatusesResult()
     class Failure(val error: String) : StatusesResult()
 }
+
+sealed class StatusesUpdateResult {
+    object Success : StatusesUpdateResult()
+    class Failure(val error: String) : StatusesUpdateResult()
+}
